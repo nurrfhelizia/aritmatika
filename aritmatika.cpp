abdcbf115839dedf7bf45b9d4579d88b6d6bc731
nurrfhelizia <144641820+nurrfhelizia@users.noreply.github.com>
2023-09-21 16:13:43 +0700

    Create aritmatika.cpp

---
+++ aritmatika.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std ;
+int main ( ) {
+	  int a, b;
+	  cout<<"PEMROGRAMAN OPERASI ARITMATIKA \n";
+	  cout<<"================ \n";
+	  cout<<"masukan angka pertama: " ;
+	  cin>>a;
+	  cout<<"masukan angka kedua: ";
+	  cin>>b;
+	  cout<<"hasil Penjumlahan: "<< a + b <<endl;
+	   cout<<"hasil Pengurangan: "<< a - b <<endl;
+	    cout<<"hasil Perkalian: "<< a * b <<endl;
+	     cout<<"hasil Pembagian: "<< a / b <<endl;
+	      cout<<"hasil Modulus: "<< a  % b <<endl; return 0; }
